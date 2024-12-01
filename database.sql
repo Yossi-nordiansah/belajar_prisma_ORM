21796c5ec2f017485d5b5e5986a3b34b8952cbcb
Yossi-nordiansah <yossi.nordiansah99@gmail.com>
2024-12-01 21:59:38 +0700

    pembuatan model pada prisma

--- database.sql
+++ database.sql
@@ -5,3 +5,13 @@ create table sample (
 )engine innodb;
 
 SELECT * FROM sample;
+
+create table customers (
+    id VARCHAR(100) NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR (100) NOT NULL,
+    phone VARCHAR (100) NOT NULL,
+    PRIMARY KEY (id),
+    CONSTRAINT customers_email_unique UNIQUE (email),
+    CONSTRAINT customers_phone_unique UNIQUE (phone)
+) ENGINE InnoDB;
