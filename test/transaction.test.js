6dd6cddc31e42a38237b0c0101a8cc7d883d5ae4
Yossi-nordiansah <yossi.nordiansah99@gmail.com>
2024-12-02 23:28:47 +0700

    add interactive transaction

--- test/transaction.test.js
+++ test/transaction.test.js
@@ -1,6 +1,6 @@
 import prismaClient from '../src/prisma-client';
 
-describe("Prisma Client", ()=>{
+describe("for sequential transaction", ()=>{
     it("sequential transaction", async () => {
        const [yossi, eva] = await prismaClient.$transaction([
             prismaClient.customer.create({
@@ -25,4 +25,29 @@ describe("Prisma Client", ()=>{
         expect(eva.name).toBe("Eva Maulinda");
         console.log(yossi)
     });
+});
+
+describe("for interactive transaction", () => {
+    it("interctive transaction", async () => {
+        const [Yossi, Eva] = await prismaClient.$transaction(async (prisma) => {
+            const Yossi = await prismaClient.customer.create({
+                data:{
+                    id: "47yr835umt",
+                    email: "yossi.nordiansah99@gmail.com",
+                    name: "Yossi Nordiansah",
+                    phone: "085655230897"
+                }
+            });
+            const Eva = await prismaClient.customer.create({
+                data: {
+                    id: "47yr835uht",
+                    email: "yossi.nordiansah07@gmail.com",
+                    name: "Eva Maulinda",
+                    phone: "085645551560"
+                }
+            });
+
+            return [Yossi, Eva];
+        })
+    })
 })
